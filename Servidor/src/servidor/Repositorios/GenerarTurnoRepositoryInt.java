b5102211ab91e5b22b103fb55e8215e5a2ead535
AIQuira <166058744+AIQuira@users.noreply.github.com>
2025-04-08 12:47:39 -0500

    Servidor eliminado

--- Servidor/src/servidor/Repositorios/GenerarTurnoRepositoryInt.java
+++
@@ -1,7 +0,0 @@
-package servidor.Repositorios;
-
-import servidor.DTO.NodoTurnoDTO;
-
-public interface GenerarTurnoRepositoryInt {
-    public NodoTurnoDTO generarTurno(String identificacion);
-}
--- Servidor/src/servidor/controladores/ControladorGeneradorTurnoInt.java
+++
@@ -1,9 +0,0 @@
-package servidor.controladores;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import servidor.DTO.NodoTurnoDTO;
-
-public interface ControladorGeneradorTurnoInt extends Remote{
-    public NodoTurnoDTO generarTurno (String identificacion) throws RemoteException;
-}
