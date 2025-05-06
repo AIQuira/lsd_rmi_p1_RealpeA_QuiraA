d97ba10a8ee7f1e8a23a4757aab72b07781fddc6
AIQuira <166058744+AIQuira@users.noreply.github.com>
2025-05-06 07:27:56 -0500

    Revert "Administrador Sistema"
    This reverts commit f811aa19177e7dc79b345f2ea560c1144d748ee2.

--- AdministradorSistema/src/cliente/controladores/ControladorCallBackAdminInt.java
+++
@@ -1,8 +0,0 @@
-package cliente.controladores;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ControladorCallBackAdminInt extends Remote{
-    public void notificarModulosOcupados(String mensaje) throws RemoteException;
-}
